d344580bee24ee96f208d3fb391e12237b0e9047
Femi Oni <freefony@gmail.com>
2018-11-24 12:36:22 +0100

    chore: assert with toBe for stricter checks

--- src/utils.test.js
+++ src/utils.test.js
@@ -12,21 +12,21 @@ describe('utility functions', () => {
     it('should return very good weather by default', () => {
       const weather = getIndicatorLevel()
       expect(weather).toBeDefined()
-      expect(weather).toEqual(VERY_GOOD_WEATHER)
+      expect(weather).toBe(VERY_GOOD_WEATHER)
     })
 
     it('should return good weather indicator for value between 50-75', () => {
       const weather = getIndicatorLevel(65)
       const weather50 = getIndicatorLevel(50)
-      expect(weather).toEqual(GOOD_WEATHER)
-      expect(weather50).toEqual(GOOD_WEATHER)
+      expect(weather).toBe(GOOD_WEATHER)
+      expect(weather50).toBe(GOOD_WEATHER)
     })
 
     it('should return poor weather for values between 25-50', () => {
       const weather25 = getIndicatorLevel(25)
       const weather49 = getIndicatorLevel(49)
-      expect(weather25).toEqual(POOR_WEATHER)
-      expect(weather49).toEqual(POOR_WEATHER)
+      expect(weather25).toBe(POOR_WEATHER)
+      expect(weather49).toBe(POOR_WEATHER)
     })
 
     it('should return extreme weather for values less than 25', () => {
