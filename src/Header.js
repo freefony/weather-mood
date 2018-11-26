5090481df0d1349f25981aca646738012af33278
Femi Oni <freefony@gmail.com>
2018-11-26 07:46:45 +0100

    feat: header and controls

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default ({cities, activeCity, switchCity}) => {
+  const isActive = (city) => (city === activeCity) ? 'active-tab': ''
+
+  const citiesEl = cities.map((city, i) => <button 
+    key={i} 
+    onClick={() => switchCity(city)}
+    className={isActive(city)}
+    >{city}</button>)
+
+  return <div className='wm-header-wrapper'>
+    <div className='wm-app-name'>Weather Mood</div>
+    <div className='wm-app-controls'>
+      {citiesEl}
+    </div>
+  </div>
+}
